ea83b8e560f72b670669b1f780323bb9d1fa1e09
DivanshuChoudhary <choudharydivanshu75@gmail.com>
2026-06-21 19:04:47 +0530

    add scroll to top button

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ buttons.forEach((btn) => {
         btn.style.background = "green";
     });
 });
+
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+        scrollBtn.style.display = "block";
+    }else{
+        scrollBtn.style.display = "none";
+    }
+
+});
+
+scrollBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+
+});
